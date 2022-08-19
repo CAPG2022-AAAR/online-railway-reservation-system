cc9dd239bbb2e6d8cb6a3ceb1eef8f156c549c06
Anurag10300 <anurag.panwar.2001@gmail.com>
2022-08-19 14:55:21 +0530

    initiate all microservice development

---
+++ available-train-info/src/test/java/com/orrs/availabletraininfo/AvailableTrainInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orrs.availabletraininfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvailableTrainInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
