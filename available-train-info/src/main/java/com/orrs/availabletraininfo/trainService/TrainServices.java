2dc7f8ab10550cc8f24b5e886bf9fd610cee518b
abhishekupadhyay260 <abhishekupadhyay260@gmail.com>
2022-08-28 19:55:05 +0530

    done crud operation in train details

---
+++ available-train-info/src/main/java/com/orrs/availabletraininfo/trainService/TrainServices.java
@@ -0,0 +1,28 @@
+package com.orrs.availabletraininfo.trainService;
+
+import com.orrs.availabletraininfo.trainDetails.TrainRequest;
+import com.orrs.availabletraininfo.trainDetails.Train_details;
+import com.orrs.availabletraininfo.trainDetailsRepository.TrainDetailsRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TrainServices {
+
+    @Autowired
+    TrainDetailsRepository trainDetailsRepository;
+
+    // saving a specific record by using the method save() of crud repository
+    public Train_details saveTrainDetails(TrainRequest train_details) {
+        return trainDetailsRepository.save(new Train_details(
+                train_details.getT_name(),
+                train_details.getT_coach(),
+                train_details.getT_seat(),
+                train_details.getT_fare(),
+                train_details.getS_time(),
+                train_details.getE_time()
+                ));
+    }
+}
