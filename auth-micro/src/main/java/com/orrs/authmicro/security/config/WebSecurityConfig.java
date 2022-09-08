d3eb229fcd5ee8550cbc5f0665e17990502a9b98
AmanSrivas <amansrivas112@gmail.com>
2022-09-08 23:10:49 +0530

    updated rabbit mq

--- auth-micro/src/main/java/com/orrs/authmicro/security/config/WebSecurityConfig.java
+++ auth-micro/src/main/java/com/orrs/authmicro/security/config/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/authenticate").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/publish/**").permitAll()
                 .antMatchers("/admin/check-connection").permitAll()
                 .antMatchers("/api/v*/registration/**").permitAll()
